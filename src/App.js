177a0fdf15804fe377f5f44a983a81867d5a835d
HouceinBenjeddou <hou52cine@gmail.com>
2022-10-30 19:09:01 +0100

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import './index.css'
 
-function App() {
+import {Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers'
+import {Article, Brand, CTA, Feature, Navbar} from './components'
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="gradient__bg">
+          <Navbar />
+          <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import google from '../../assets/google.png'
+import slack from '../../assets/slack.png'
+import atlassian from '../../assets/atlassian.png'
+import shopify from '../../assets/shopify.png'
+import dropbox from '../../assets/dropbox.png'
+
+    export {
+        google,
+        slack,
+        atlassian,
+        shopify,
+        dropbox,
+    }
