26bd23a5e3ff60cfc2fc6f4b9b2bfcc5fbce63a8
HouceinBenjeddou <hou52cine@gmail.com>
2022-11-01 11:39:17 +0100

    brandchange

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -5,19 +5,19 @@ import './brand.css';
 const Brand = () => (
   <div className="gpt3__brand section__padding">
     <div>
-      <img src={google} />
+      <img src={google}  alt="img"/>
     </div>
     <div>
-      <img src={slack} />
+      <img src={slack} alt="img"/>
     </div>
     <div>
-      <img src={atlassian} />
+      <img src={atlassian} alt="img"/>
     </div>
     <div>
-      <img src={dropbox} />
+      <img src={dropbox} alt="img"/>
     </div>
     <div>
-      <img src={shopify} />
+      <img src={shopify} alt="img"/>
     </div>
   </div>
 );
