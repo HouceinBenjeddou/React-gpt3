db5ba247671c37f1db6b54a1ed3355f18aa8d420
HouceinBenjeddou <hou52cine@gmail.com>
2022-11-01 11:14:27 +0100

    update

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Feature from '../../components/feature/Feature';
-import './whatGPT3.css';
+import './whatgpt3.css';
 
 const WhatGPT3 = () => (
   <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
